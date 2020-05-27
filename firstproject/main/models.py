5828e0822ea5c232822eafb902bc1c0231409efc
SavvyLogix <danylov.alex02@gmail.com>
2020-05-27 16:27:37 +0300

    first commit

---
+++ firstproject/main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Advert(models.Model):
+    title = models.CharField(verbose_name='заглавие', max_length=20)
+    text = models.TextField(verbose_name='Текст обьявления', null=True)
+    phone = models.CharField(verbose_name='Телефон', max_length=15, null=True, blank=True)
+    email = models.EmailField(verbose_name='Email', max_length=30, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Обьявление'
+        verbose_name_plural = 'Обьявления'
+
